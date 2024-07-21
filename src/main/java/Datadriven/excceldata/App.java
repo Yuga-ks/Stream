9a827b26b2b74cec6cec665e75a823afd84049e2
Harry <yugheshwara@gmail.com>
2024-07-22 01:46:07 +0530

    Uploading the data from excel

---
+++ src/main/java/Datadriven/excceldata/App.java
@@ -0,0 +1,13 @@
+package Datadriven.excceldata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
